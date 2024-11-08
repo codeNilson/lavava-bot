74dd6f2fa693ecfcd32779b6b3e230eb738c25d9
Denilson <fcodenilson@gmail.com>
2024-11-08 07:06:57 -0300

    refactor player command to use file attachments and simplify player model initialization

--- core/cogs/players.py
+++ core/cogs/players.py
@@ -9,16 +9,17 @@ class Players(commands.Cog, name="PlayersCog"):
 
     @commands.command()
     async def player(self, ctx, player: Player):
-        embed = discord.Embed(
-            title=player.username, description=player.uuid, color=discord.Color.red()
+        embed = discord.Embed(title=player.username, color=discord.Color.red())
+
+        file = discord.File(
+            "C:/Users/Denilson/Documents/GitHub/valorant-amateur-league/gamedata/static/gamedata/assets/agents/icons/omen_display_icon.webp",
+            filename="omen_display_icon.webp",
         )
-        embed.set_thumbnail(url="http://127.0.0.1:8000/static/" + player.tier.small_icon)
+        embed.set_thumbnail(url="attachment://omen_display_icon.webp")
 
-        url = "http://127.0.0.1:8000/static/" + player.tier.small_icon
-        print(url)
-
-        embed.add_field(name="Username", value=player.username)
-        embed.add_field(name="Email", value=player.email)
+        embed.add_field(
+            name="Main Agent", value=player.main_agent["name"], inline=False
+        )
+        embed.add_field(name="Ranking", value=player.ranking)
         embed.add_field(name="Tier", value=player.tier.tier)
-        embed.add_field(name="Division", value=player.tier.division)
-        await ctx.send(embed=embed)
+        await ctx.send(file=file, embed=embed)
