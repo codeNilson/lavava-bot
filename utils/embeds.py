f527b4299d533a762ce2d6bc06f5a1f5d379ad7a
denilson <fcodenilson@gmail.com>
2024-12-22 14:14:48 -0300

    feat: enhance player display by using username as fallback for missing UID

--- utils/embeds.py
+++ utils/embeds.py
@@ -3,8 +3,8 @@ import discord
 
 async def show_teams(ctx, team_a, team_b):
 
-    players_team_a = [f"⚔️ <@{player.discord_uid}>" for player in team_a.players]
-    players_team_b = [f"🛡️ <@{player.discord_uid}>" for player in team_b.players]
+    players_team_a = [f"<@{player.discord_uid or player.username}>" for player in team_a.players]
+    players_team_b = [f"<@{player.discord_uid or player.username}>" for player in team_b.players]
 
     embed = discord.Embed(
         title="Times escolhidos!",
